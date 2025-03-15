d2745b22230d0d1aeb2d7a0c7918a0ef5b290569
olaQ234 <oladimejiquadri10@gmail.com>
2025-03-15 17:54:24 -0400

    Update roleconfig.sh

--- roleconfig.sh
+++ roleconfig.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Creating our IAM Role 'MyAutomationRole', required for AWS Config auto remediation"
-echo "Attaching the the AmazonSSMAutomationRole AWS Managed Policy..."
-#This command creates our role and configures a trust policy which allows EC2 and Systems Manager to assume the role.
-echo "Creating the role and configuring the Trust Policy, to allow EC2 and Systems Manager to assume this role..."
+echo "Attaching the AmazonSSMAutomationRole AWS Managed Policy..."
+
+# Creating the role and configuring the Trust Policy
+echo "Creating the role and configuring the Trust Policy..."
 iamRoleArn=$(aws iam create-role --role-name "MyAutomationRole" \
 --assume-role-policy-document "$(cat <<EOF
 {
@@ -23,27 +24,33 @@ iamRoleArn=$(aws iam create-role --role-name "MyAutomationRole" \
     ]
 }
 EOF
-)"
- --query "Role.Arn" --output=text)
+)" --query "Role.Arn" --output=text)
 
-#This command attaches the AmazonSSMAutomationRole AWS Managed Policy 
-echo "Attaching the the AmazonSSMAutomationRole AWS Managed Policy..."
+# Error Handling: If IAM Role creation failed, exit
+if [[ -z "$iamRoleArn" ]]; then
+    echo "Error: IAM Role creation failed!"
+    exit 1
+fi
+
+# Attaching the AmazonSSMAutomationRole AWS Managed Policy
+echo "Attaching the AmazonSSMAutomationRole AWS Managed Policy..."
 aws iam attach-role-policy --role-name "MyAutomationRole" \
  --policy-arn "arn:aws:iam::aws:policy/service-role/AmazonSSMAutomationRole"
 
-#This command adds the inline policy which allows the role to be passed to another service. We have to pass in the ARN of our role. 
+# Adding the inline policy which allows the role to be passed to another service
 echo "Adding the inline policy which allows the role to be passed to another service..."
 aws iam put-role-policy --role-name "MyAutomationRole" \
  --policy-name "AllowPassRole" \
- --policy-document '{
-    "Version": "2012-10-17",
-    "Statement": [
+ --policy-document "{
+    \"Version\": \"2012-10-17\",
+    \"Statement\": [
         {
-            "Effect": "Allow",
-            "Action": "iam:PassRole",
-            "Resource": "'$iamRoleArn'"
+            \"Effect\": \"Allow\",
+            \"Action\": \"iam:PassRole\",
+            \"Resource\": \"$iamRoleArn\"
         }
     ]
-}'
-#Finally, we'll output the ARN of the role we created
-echo $iamRoleArn
+}"
+
+# Finally, output the ARN of the role we created
+echo "IAM Role successfully created! ARN: $iamRoleArn"
